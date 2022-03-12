f0ec683e5f2fc39a89d0d72092ec8c348c896380
naoto-naka <luckywhitecat770@gmail.com>
2022-03-12 21:36:58 +0900

    vue routerなどのミドルウェアが入っていなかったため vue プロジェクトを作り直しました

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,24 +1,19 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { RouterLink, RouterView } from "vue-router";
+  import Header from "@/components/Header.vue";
+  import Footer from "@/components/Footer.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <Header />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <RouterView />
 
-  <main>
-    <TheWelcome />
-  </main>
+  <Footer />
 </template>
 
 <style>
-@import './assets/base.css';
+@import "@/assets/base.css";
 
 #app {
   max-width: 1280px;
@@ -30,6 +25,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 header {
   line-height: 1.5;
+  max-height: 100vh;
 }
 
 .logo {
@@ -48,6 +44,31 @@ a,
   a:hover {
     background-color: hsla(160, 100%, 37%, 0.2);
   }
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
 }
 
 @media (min-width: 1024px) {
@@ -76,6 +97,15 @@ a,
 
   .logo {
     margin: 0 2rem 0 0;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
   }
 }
 </style>
---
+++ front/src/components/Header.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <div>
+      <RouterLink to="/">WebXam</RouterLink>
+    </div>
+
+    <nav>
+      <RouterLink to="#">ユーザー登録</RouterLink>
+      <RouterLink to="#">ユーザー一覧</RouterLink>
+      <RouterLink to="#">問題一覧</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style>
+</style>
